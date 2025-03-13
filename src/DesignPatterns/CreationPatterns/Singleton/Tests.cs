5467944516f3d817fa3d87a4fb0d766830b5b3eb
brucengdev <brucengdev@gmail.com>
2025-03-13 12:57:12 +0700

    wip

--- src/DesignPatterns/CreationPatterns/Singleton/Tests.cs
+++ src/DesignPatterns/CreationPatterns/Singleton/Tests.cs
@@ -5,6 +5,8 @@ public class Tests
     [Fact]
     public void Test1()
     {
+        //currently, MySingleton's static constructor is not triggered
+        //so this test is failing.
         var instance = Singleton.Instance();
         
         instance.GetValue().ShouldBe("This is from MySingleton");

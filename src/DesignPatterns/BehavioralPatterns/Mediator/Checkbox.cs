2c71b8dcce21f9058cef198c4a31a2b55a28ab59
brucengdev <brucengdev@gmail.com>
2025-08-05 13:29:46 +0700

    wip

---
+++ src/DesignPatterns/BehavioralPatterns/Mediator/Checkbox.cs
@@ -0,0 +1,8 @@
+namespace Mediator;
+
+public class Checkbox
+{
+    public Checkbox(string label, DishDialogDirector director)
+    {
+    }
+}
--- src/DesignPatterns/BehavioralPatterns/Mediator/Tests.cs
+++ src/DesignPatterns/BehavioralPatterns/Mediator/Tests.cs
@@ -3,11 +3,12 @@ namespace Mediator;
 public class Tests
 {
     [Fact]
-    public void Test1()
+    public void Test_dish_selector_dialog()
     {
         var director = new DishDialogDirector();
         var dishName = new TextField("Dish Name", director);
         var dishList = new ListField("Dish List", director);
 
+        var discountCheckbox = new Checkbox("Discount", director);
     }
 }
